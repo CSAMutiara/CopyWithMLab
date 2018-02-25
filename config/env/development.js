9fe26c41c0cb3296ca43b81fbb829b3b83993e34
Ayesha Mutiara <ayesha.mutiara@outlook.com>
2018-02-25 09:22:07 +0000

    changed dburi

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db_uri: 'mongodb://amutiara-cs3051-5326061:27017/cs3051',
+    db_uri: 'mongodb://admin:admin@ds247688.mlab.com:47688/linklibapp',
     db: function(callback) {
         var MongoClient = require('mongodb').MongoClient
         MongoClient.connect(this.db_uri, callback)
